20d9d3a088912bb66f43970b67a6821512b32c5b
Jaime Pérez Castellanos <jaipecas89@gmail.com>
2025-02-20 19:15:06 +0100

    fte: add use form in user page

--- src/virtualLibrary/pages/UserPage.jsx
+++ src/virtualLibrary/pages/UserPage.jsx
@@ -1,15 +1,27 @@
-import { Grid2, Typography, Box, TextField } from "@mui/material";
+import { Grid2, Typography, Box, TextField, Button } from "@mui/material";
 import profileMan from "../../assets/images/profileMan.png";
 import profileWoman from "../../assets/images/profileWoman.png";
 import previous from "../../assets/images/previous.png";
 import next from "../../assets/images/next.png";
-import { useState } from "react";
+import { useContext, useEffect, useState } from "react";
+import { useForm } from "../../hooks/useForm";
+import { AuthContext } from "../../auth/context/AuthContext";
 
 const logos = [profileMan, profileWoman];
 
 export const UserPage = () => {
   const [showPassword, setShowPassword] = useState(false);
   const [selectedLogo, setSelectedLogo] = useState(0);
+  const { formState, onInputChange, onFormSubmitted } = useForm(
+    {},
+    true
+  );
+
+  const onUpdateUserFormSubmitted = (event) => {
+    const isSubmitted = onFormSubmitted(event);
+
+    if (isSubmitted) onregister();
+  };
 
   const onClickShowPass = () => setShowPassword((prev) => !prev);
 
@@ -28,112 +40,140 @@ export const UserPage = () => {
   };
 
   return (
-    <Grid2
-      container
-      sx={{ mt: 10 }}
-      justifyContent="center"
-      alignItems="center"
-    >
+    <form onSubmit={onUpdateUserFormSubmitted}>
       <Grid2
         container
-        size={{ xs: 12, md: 6 }}
+        sx={{ mt: 10 }}
         justifyContent="center"
         alignItems="center"
       >
-        <Grid2 size={{ xs: 12 }} container justifyContent="center" spacing={2}>
-          <Box
-            component="img"
-            src={logos[selectedLogo]}
-            alt="Logo"
-            sx={{
-              width: "30%",
-              height: "auto",
-              objectFit: "contain",
-            }}
-          />
-        </Grid2>
         <Grid2
-          size={{ xs: 12 }}
-          sx={{ mt: 3 }}
           container
+          size={{ xs: 12, md: 6 }}
           justifyContent="center"
-          spacing={2}
+          alignItems="center"
         >
-          <Box
-            component="img"
-            src={previous}
-            alt="Logo"
-            sx={{
-              width: "7%",
-              height: "auto",
-              objectFit: "contain",
-              cursor: "pointer",
-            }}
-            onClick={onPreviousLogo}
-          />
-          <Box
-            component="img"
-            src={next}
-            alt="Logo"
-            sx={{
-              width: "7%",
-              height: "auto",
-              objectFit: "contain",
-              cursor: "pointer",
-            }}
-            onClick={onNextLogo}
-          />
+          <Grid2
+            size={{ xs: 12 }}
+            container
+            justifyContent="center"
+            spacing={2}
+          >
+            <Box
+              component="img"
+              src={logos[selectedLogo]}
+              alt="Logo"
+              sx={{
+                width: "30%",
+                height: "auto",
+                objectFit: "contain",
+              }}
+            />
+          </Grid2>
+          <Grid2
+            size={{ xs: 12 }}
+            sx={{ mt: 3 }}
+            container
+            justifyContent="center"
+            spacing={2}
+          >
+            <Box
+              component="img"
+              src={previous}
+              alt="Logo"
+              sx={{
+                width: "7%",
+                height: "auto",
+                objectFit: "contain",
+                cursor: "pointer",
+              }}
+              onClick={onPreviousLogo}
+            />
+            <Box
+              component="img"
+              src={next}
+              alt="Logo"
+              sx={{
+                width: "7%",
+                height: "auto",
+                objectFit: "contain",
+                cursor: "pointer",
+              }}
+              onClick={onNextLogo}
+            />
+          </Grid2>
+        </Grid2>
+        <Grid2 container size={{ xs: 12, md: 6 }} spacing={2}>
+          <Grid2 size={{ xs: 10 }}>
+            <Typography variant="h3">Perfil</Typography>
+          </Grid2>
+          <Grid2 size={{ xs: 10 }}>
+            <TextField
+              name="userName"
+              label="UserName"
+              type="text"
+              placeholder="userName"
+              onChange={onInputChange}
+              fullWidth
+            />
+          </Grid2>
+          <Grid2 size={{ xs: 10 }}>
+            <TextField
+              name="email"
+              label="Correo"
+              type="email"
+              placeholder="user@email.com"
+              onChange={onInputChange}
+              fullWidth
+              error={formState.emailError}
+              helperText={formState.emailError ? formState.emailError : ""}
+            />
+          </Grid2>
+          <Typography
+            variant="body2"
+            color="primary"
+            sx={{ cursor: "pointer" }}
+            onClick={onClickShowPass}
+          >
+            ¿Quieres cambiar tu contraseña?
+          </Typography>
+          <Grid2 size={{ xs: 10 }} sx={{ display: showPassword ? "" : "none" }}>
+            <TextField
+              name="password"
+              label="Contraseña"
+              type="password"
+              placeholder="Contraseña"
+              onChange={onInputChange}
+              fullWidth
+              error={formState.passwordError ? true : false}
+              helperText={
+                formState.passwordError ? formState.passwordError : ""
+              }
+            />
+          </Grid2>
+          <Grid2 size={{ xs: 10 }} sx={{ display: showPassword ? "" : "none" }}>
+            <TextField
+              name="confirmPassword"
+              label="Confirmar contraseña"
+              type="password"
+              placeholder="Contraseña"
+              onChange={onInputChange}
+              fullWidth
+              error={formState.confirmPasswordError ? true : false}
+              helperText={
+                formState.confirmPasswordError
+                  ? formState.confirmPasswordError
+                  : ""
+              }
+            />
+          </Grid2>
+          <Grid2 size={{ xs: 10 }} sx={{ mt: 2 }}>
+            <Button type="submit" variant="contained" fullWidth>
+              Actualizar
+            </Button>
+          </Grid2>
         </Grid2>
       </Grid2>
-      <Grid2 container size={{ xs: 12, md: 6 }} spacing={2}>
-        <Grid2 size={{ xs: 10 }}>
-          <Typography variant="h3">Perfil</Typography>
-        </Grid2>
-        <Grid2 size={{ xs: 10 }}>
-          <TextField
-            name="userName"
-            label="UserName"
-            type="text"
-            placeholder="Usernname"
-            fullWidth
-          />
-        </Grid2>
-        <Grid2 size={{ xs: 10 }}>
-          <TextField
-            name="email"
-            label="Correo"
-            type="text"
-            placeholder="correo@google.com"
-            fullWidth
-          />
-        </Grid2>
-        <Typography
-          variant="body2"
-          color="primary"
-          sx={{ cursor: "pointer" }}
-          onClick={onClickShowPass}
-        >
-          ¿Quieres cambiar tu contraseña?
-        </Typography>
-        <Grid2 size={{ xs: 10 }} sx={{ display: showPassword ? "" : "none" }}>
-          <TextField
-            name="password"
-            label="Contraseña"
-            type="text"
-            placeholder="contraseña"
-            fullWidth
-          />
-        </Grid2>
-        <Grid2 size={{ xs: 10 }} sx={{ display: showPassword ? "" : "none" }}>
-          <TextField
-            name="confirmPassword"
-            label="Confirmar contraseña"
-            type="text"
-            placeholder="contraseña"
-            fullWidth
-          />
-        </Grid2>
-      </Grid2>
-    </Grid2>
+    </form>
   );
 };
