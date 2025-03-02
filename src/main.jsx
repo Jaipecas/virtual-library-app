54b3ae606fee5fb3f4c3c2bc0d7a105bf4a923cc
Jaime Pérez Castellanos <jaipecas89@gmail.com>
2025-03-02 14:09:39 +0100

    fte: finish studyRoomDashborad

--- src/main.jsx
+++ src/main.jsx
@@ -1,3 +1,4 @@
+import { CssBaseline } from "@mui/material";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
@@ -6,6 +7,7 @@ import { VirtualLibraryApp } from "./VirtualLibraryApp";
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
+      <CssBaseline />
       <VirtualLibraryApp />
     </BrowserRouter>
   </StrictMode>
