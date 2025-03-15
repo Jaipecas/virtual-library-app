42f3fcc1bf16a44335e4bd68a3083e4b9171dd11
Jaime Pérez Castellanos <jaipecas89@gmail.com>
2025-03-15 13:16:35 +0100

    fte: create crud studyRoom

--- src/services/virtualLibraryApi.js
+++ src/services/virtualLibraryApi.js
@@ -4,16 +4,44 @@ export const virtualLibraryApi = createApi({
   reducerPath: "virtualLibraryApi",
 
   baseQuery: fetchBaseQuery({ baseUrl: "https://localhost:44347/api" }),
+  tagTypes: ["StudyRooms"],
 
-  endpoints: (build) => ({
-    addStudyRoom: build.mutation({
+  endpoints: (builder) => ({
+
+    //StudRoom
+    addStudyRoom: builder.mutation({
       query: (newRoom) => ({
-        url: "/studyRoom",
+        url: "/studyroom",
         method: "POST",
         body: newRoom,
       }),
+      invalidatesTags: ["StudyRooms"],
+    }),
+    getStudyRooms: builder.query({
+      query: () => "/studyroom",
+      providesTags: ["StudyRooms"],
+    }),
+    updateStudyRoom: builder.mutation({
+      query: ({ id, updatedRoom }) => ({
+        url: `/studyroom/${id}`,
+        method: "PUT",
+        body: updatedRoom,
+      }),
+      invalidatesTags: ["StudyRooms"],
+    }),
+    deleteStudyRoom: builder.mutation({
+      query: (id) => ({
+        url: `/studyroom/${id}`,
+        method: "DELETE",
+      }),
+      invalidatesTags: ["StudyRooms"],
     }),
   }),
 });
 
-export const { useAddStudyRoomMutation } = pokemonApi;
+export const {
+  useGetStudyRoomsQuery,
+  useAddStudyRoomMutation,
+  useUpdateStudyRoomMutation,
+  useDeleteStudyRoomMutation,
+} = pokemonApi;
