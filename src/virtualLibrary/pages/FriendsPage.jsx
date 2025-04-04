2f0b74ba13ad72e3a032dc022140f7b7d25c51ed
Jaime Pérez Castellanos <jaipecas89@gmail.com>
2025-04-04 20:00:07 +0200

    fte: refactor friendPage

--- src/virtualLibrary/pages/FriendsPage.jsx
+++ src/virtualLibrary/pages/FriendsPage.jsx
@@ -1,16 +1,16 @@
-import { Alert, Avatar, Box, Card, CardHeader, CardMedia, IconButton, InputAdornment, Paper, TextField, Typography } from '@mui/material'
+import { Alert, Avatar, Box, Card, CardHeader, IconButton, InputAdornment, Paper, TextField, Typography } from '@mui/material'
 import SearchIcon from "@mui/icons-material/Search";
 import { useState } from 'react';
 import profileMan from "../../assets/images/profileMan.png";
 import { useDispatch, useSelector } from 'react-redux';
-import { getUserData, updateFriend } from '../../store/thunks/userThunks';
+import { getUserData } from '../../store/thunks/userThunks';
 import { useEffect } from 'react';
 import { sendNotificationsThunk } from '../../store/thunks/notificationThunks';
 
 export const FriendsPage = () => {
     const [search, setSearch] = useState("");
     const dispatch = useDispatch();
-    const { userData, loading, error } = useSelector(state => state.user);
+    const { userData } = useSelector(state => state.user);
     const { sendSuccess, error: notificationError } = useSelector(state => state.notifications);
     const { user } = useSelector(state => state.auth);
 
@@ -56,7 +56,7 @@ export const FriendsPage = () => {
             </form>
 
             {notificationError && <Alert sx={{ marginTop: 1 }} severity="error">{notificationError}</Alert>}
-            {sendSuccess && <Alert sx={{ marginTop: 1 }} severity="success">{sendSuccess}</Alert>}      
+            {sendSuccess && <Alert sx={{ marginTop: 1 }} severity="success">{sendSuccess}</Alert>}
 
             <Box marginTop={2} display="flex" flexWrap="wrap" gap={3}>
                 {userData.friends.map(friend => (
